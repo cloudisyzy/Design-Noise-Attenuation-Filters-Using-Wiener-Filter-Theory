8a3a4112e5e5e986ae2bada586a89f90d8858ad4
cloudisyzy <yangzy0516@outlook.com>
2024-01-27 13:23:23 +0100

    -

--- estimation_noise.m
+++ estimation_noise.m
@@ -1,4 +1,7 @@
 clear 
+
+addpath('mfiles\')
+addpath('skel\')
 [z,fs] = audioread('EQ2401project1data2024.wav');
 
 estimated_order = 19;
@@ -6,19 +9,17 @@ estimated_order = 19;
 % figure;
 % plot(z)
 
-z1 = z(1:4700);
-z2 = z(18900:26500);
-z3 = z(41450:51000);
-z = [z1;z2;z3];
+y = z([4501:18999 26001:41999 51001:length(z)]);
+% sound(z, fs)
 
 % [pxx, f_welch] = pwelch(z, hamming(64), 32, 512, 1, 'twosided');
-[pxx, f_welch] = pwelch(z, hamming(64), 32, 1024, 1, 'twosided');
+[pxx, f_welch] = pwelch(y, hamming(64), 32, 1024, 1, 'twosided');
 f_welch = f_welch - 0.5;
 N = length(f_welch);
 halfN = floor(N/2);
 pxx = [pxx(halfN+1:end); pxx(1:halfN)];
 
-[Ahat, sigma2hat] = ar_id(z, estimated_order);
+[Ahat, sigma2hat] = ar_id(y, estimated_order);
 f_theory = linspace(-0.5, 0.5, 1000);
 H = freqz(1, Ahat, 2*pi*f_theory);
 S_y = abs(H).^2 * sigma2hat;
