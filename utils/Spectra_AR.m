4af230374d731dc6bfafd375ab4db0484db028ec
cloudisyzy <yangzy0516@outlook.com>
2024-01-27 11:50:30 +0100

    two funcs for plotting

---
+++ utils/Spectra_AR.m
@@ -0,0 +1,33 @@
+function [p_AR, H_AR, f] = Spectra_AR(A, sigma2, Option, plotFlag)
+%   Compute theoretical PSD of AR model given
+%   A - denominator A(q)
+%   sigma2 - variance of AR input noise
+%   Option ='full', or 'half'
+%   plotFlag - =1, plot; =0, do not plot
+%   p_AR - theoretical PSD for given AR parameters
+%   H_AR - frequency response of AR transfer func
+%   f - nomarlized freq, from -0.5 to 0.5
+%   Author: Ziyue Yang
+%   Date: 2024.01.27
+
+    f = linspace(-0.5, 0.5, 2048);
+    H_AR = freqz(1, A, 2*pi*f);
+    p_AR = abs(H_AR).^2 * sigma2;
+
+    if strcmp(Option, 'full')
+
+    elseif strcmp(Option, 'half')
+        N = length(f);
+        halfN = floor(N/2);
+        f = f(halfN+1:end);
+        H_AR = H_AR(halfN+1:end);
+        p_AR = p_AR(halfN+1:end);
+     
+    end
+    
+    if plotFlag
+        semilogy(f, p_AR, 'b--')   
+    end
+
+end
+
---
+++ utils/Spectra_Est.m
@@ -0,0 +1,29 @@
+function [pxx, f] = Spectra_Est(x, Option, plotFlag)
+%   Using Welch Method to estimate the spectrum of given signals and give
+%   plot
+%   x - signal to be estimated
+%   Option ='full', or 'half'
+%   plotFlag =1, plot; =0, do not plot
+%   pxx - estimated spectrum
+%   f - nomarlized freq, from -0.5 to 0.5
+%   Author: Ziyue Yang
+%   Date: 2024.01.27
+
+    [pxx, f] = pwelch(x, hamming(64), 32, 2048, 1, 'twosided');
+    N = length(f);
+    halfN = floor(N/2);
+
+    if strcmp(Option, 'full')
+        f = f - 0.5;
+        pxx = [pxx(halfN+1:end); pxx(1:halfN)];
+    elseif strcmp(Option, 'half')
+        f = f(1:halfN);
+        pxx = pxx(1:halfN);
+    end
+
+    if plotFlag
+        semilogy(f, pxx, 'r')
+    end
+
+end
+
